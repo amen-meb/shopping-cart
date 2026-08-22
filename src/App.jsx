eef8d17ed02a8dacc8eb6940998790ddae8d6a26
Amanuel Mebratu <169341614+amen-meb@users.noreply.github.com>
2026-08-22 19:33:55 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>My Shopping Cart</h1>
+        <p>Welcome to my store</p>
+      </div>
+
+    </>
+  )
+}
+
+export default App
